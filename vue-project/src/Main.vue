7b431f09674a226a6db1652942def4aefa1bd0d5
luzi82 <luzi82@gmail.com>
2022-06-03 10:55:48 +0800

    add timer example

--- vue-project/src/Main.vue
+++ vue-project/src/Main.vue
@@ -2,14 +2,27 @@
 export default {
   data() {
     return {
-      count: 0
+      count: 0,
+      inc: 0
     }
-  }
+  },
+  methods: {
+    tick() {
+      ++this.inc
+    }
+  },
+  mounted() {
+    this.tickTimer = setInterval(this.tick, 1000);
+  },
+  beforeDestroy() {
+    clearInterval(this.tickTimer);
+  },
 }
 </script>
 
 <template>
   <button @click="count++">Count is: {{ count }}</button>
+  <div>inc = {{inc}}</div>
 </template>
 
 <style scoped>
